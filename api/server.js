8ff086217990a2e50bb01136e13847e3e60349ec
Mayank Chauhan <cmayank149@gmail.com>
2026-04-09 20:28:26 +0530

    Backend deploy updates

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 // ── Initialise Firebase Admin SDK ─────────────────────────────────────────────
 admin.initializeApp({
